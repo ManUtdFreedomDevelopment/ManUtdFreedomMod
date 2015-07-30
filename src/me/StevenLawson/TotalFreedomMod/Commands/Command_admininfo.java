4e7116580dae4ec01f4a1f17303e995a4480dfd3
DarkGamingDronze <wesam_111@hotmail.com>
2015-07-30 20:19:18 +0400

    Create Command_admininfo.java

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_admininfo.java
@@ -0,0 +1,30 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.Commands.AdminLevel;
+import me.StevenLawson.TotalFreedomMod.Commands.CommandParameters;
+import me.StevenLawson.TotalFreedomMod.Commands.CommandPermissions;
+import me.StevenLawson.TotalFreedomMod.Commands.SourceType;
+import me.StevenLawson.TotalFreedomMod.Commands.TFM_Command;
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
+@CommandParameters(
+        description = "See how to become admin.",
+        aliases = "ai",
+        usage = "/<command>")
+public class Command_admininfo extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        TFM_Util.playerMsg(sender_p, "So you wish to apply for admin do you?", ChatColor.BLUE);
+        TFM_Util.playerMsg(sender_p, "Then Visite http://diamondfreedom.boards.net for more information!", ChatColor.RED);
+        
+       
+        return true;
+       }
+}
